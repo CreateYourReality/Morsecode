5f202bad6f440d3c5b2237919e396c7b89bd256f
CreateYourReality <timo.moewes@icloud.com>
2023-05-19 11:06:53 +0200

    final

---
+++ assets/main.js
@@ -0,0 +1,65 @@
+let morseAlphabet = [
+    { letter: "1", morseCode: ".----" },
+    { letter: "2", morseCode: "..---" },
+    { letter: "3", morseCode: "...--" },
+    { letter: "4", morseCode: "....-" },
+    { letter: "5", morseCode: "....." },
+    { letter: "6", morseCode: "-...." },
+    { letter: "7", morseCode: "--..." },
+    { letter: "8", morseCode: "---.." },
+    { letter: "9", morseCode: "----." },
+    { letter: "0", morseCode: "-----" },
+    { letter: " ", morseCode: "    " },
+    { letter: "A", morseCode: ".-" },
+    { letter: "B", morseCode: "-..." },
+    { letter: "C", morseCode: "-.-." },
+    { letter: "D", morseCode: "-.." },
+    { letter: "E", morseCode: "." },
+    { letter: "F", morseCode: "..-." },
+    { letter: "G", morseCode: "--." },
+    { letter: "H", morseCode: "...." },
+    { letter: "I", morseCode: ".." },
+    { letter: "J", morseCode: ".---" },
+    { letter: "K", morseCode: "-.-" },
+    { letter: "L", morseCode: ".-.." },
+    { letter: "M", morseCode: "--" },
+    { letter: "N", morseCode: "-." },
+    { letter: "O", morseCode: "---" },
+    { letter: "P", morseCode: ".--." },
+    { letter: "Q", morseCode: "--.-" },
+    { letter: "R", morseCode: ".-." },
+    { letter: "S", morseCode: "..." },
+    { letter: "T", morseCode: "-" },
+    { letter: "U", morseCode: "..-" },
+    { letter: "V", morseCode: "...-" },
+    { letter: "W", morseCode: ".--" },
+    { letter: "X", morseCode: "-..-" },
+    { letter: "Y", morseCode: "-.--" },
+    { letter: "Z", morseCode: "--.." }
+  ];
+
+
+  const inputField = document.getElementById("input");
+  const output = document.querySelector("h1");
+  const description = document.querySelector("p");
+  
+  const encryptText = () => {
+    event.preventDefault();
+    let inputValue = inputField.value.toUpperCase();
+    let inputLength = inputField.value.length;
+
+    output.innerHTML = "";
+    description.innerHTML = "";
+
+    for(let i = 0; i < inputLength; i++){
+        morseAlphabet.forEach(code => {
+            if (code.letter == inputValue.slice(i,1+i)){
+                output.innerHTML += code.morseCode;
+                output.innerHTML += "   ";
+                description.innerHTML += inputValue.slice(i,1+i) + " = " + code.morseCode + "<br>";
+            }
+      });
+    }
+  }
+
+
